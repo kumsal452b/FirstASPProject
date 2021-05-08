e8e3204190f9e0bfa050c0b7e3a7e81ca94b59c3
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-05-08 21:16:04 +0300

    add new two element

---
+++ FirstASPProject/Scripts/myScript.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="angular.js" />
+
+var myApp = angular.module("myModule", [])
+    .controller("myController", function ($scope) {
+        var mobiles = [
+            { brand: "Samsung", likes: 0, dislikes: 0 },
+            { brand: "Xiaomi", likes: 0, dislikes: 0 },
+            { brand: "Nokia", likes: 0, dislikes: 0 }
+        ];
+
+        $scope.mobiles = mobiles;
+        $scope.likeIncrement = function (mobile) {
+            mobile.likes++;
+        };
+
+        $scope.disLikeIncrement = function (mobile) {
+            mobile.likes++;
+        };
+
+    });
