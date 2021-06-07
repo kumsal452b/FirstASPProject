3506fc065cca032cf5f426676f47ac5e071074e5
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-06-08 01:32:38 +0300

    Bazı satırlar ekledım

--- FirstASPProject/Controllers/HomeController.cs
+++ FirstASPProject/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace FirstASPProject.Controllers
 {
@@ -11,6 +7,7 @@ namespace FirstASPProject.Controllers
         public ActionResult Index()
         {
             return View();
+            //ses aramasi baslatir misin
         }
 
         public ActionResult About()
